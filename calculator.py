dc681959065790f188d00d6abb028819fdf77ea9
Tarunav BA <tarunavba@gmail.com>
2021-09-11 20:08:29 +0530

    👌 IMPROVE: calculator

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 print("Welcome to the calculator")
 
-the_first_number = input(int("Enter the first digit: "))
+the_first_number = input("Enter the first digit: ")
 the_operation = input("Enter the operation to be done +, -, /, *: ")
-the_second_number = input(int("Enter the second digit: "))
+the_second_number = input("Enter the second digit: ")
 if the_operation == "+":
-    print(the_first_number + the_second_number)
+    print(int(the_first_number) + int(the_second_number))
 if the_operation == "-":
-    print(the_first_number - the_second_number)
+    print(int(the_first_number) - int(the_second_number))
 if the_operation == "/":
-    print(the_first_number / the_second_number)
+    print(int(the_first_number) / int(the_second_number))
 if the_operation == "*":
-    print(the_first_number * the_second_number)
+    print(int(the_first_number) * int(the_second_number))
 
 print("Thank you.")
